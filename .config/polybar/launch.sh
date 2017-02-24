096008333424b10389f6b90d54c6dceec6e8ee3b
Tim Williams <tim.williams.public@gmail.com>
2017-02-24 22:45:17 +0000

    new config

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Launch my only bar so far...
+polybar bottom &
+
+echo "Bar launched..."
